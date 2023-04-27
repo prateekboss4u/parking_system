41af9384103658aa339710062c54f17124d5e56a
prateeksikarwar <prateek612002@gmail.com>
2023-04-27 21:54:22 +0530

    [BUGFIX] Resolved schema.rb conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_04_27_130633) do
   create_table "billings", force: :cascade do |t|
     t.string "customer_name", null: false
@@ -20,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_27_130633) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "locations", force: :cascade do |t|
     t.string "location_name"
     t.integer "two_wheeler_capacity"
