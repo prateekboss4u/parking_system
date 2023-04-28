e4ab85cdcf003adbfd1f16c8be6a9a6e8a79a203
prateeksikarwar <prateek612002@gmail.com>
2023-04-28 11:16:16 +0530

    Update User Validations
    Added validations in User model
    and added owner_action and operator_action in controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+    validates :username, presence: true
+    validates :type_of_user, presence: true
+    validate :role_validation
+    validates :location, presence: true
+    validates :city, presence: true
+
+
+    private
+
+    def role_validation
+        roles = ['owner', 'operator']
+        errors.add('Type of User can only be', roles.join(', ')) unless roles.include? type_of_user
+    end
+
 end
