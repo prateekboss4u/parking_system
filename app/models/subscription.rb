6bb8d2b5fbe5cc19bb7ebace32f9dedba46a2aa5
anuj <anujshukla151296@gmail.com>
2023-04-28 13:21:16 +0530

    Create validation in subscription model
    created validation for name,pass type,start & end date in subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,27 @@
 class Subscription < ApplicationRecord
+
+    validates :name, presence: true, exclusion: { in: %w(owner operator) }
+    validates :type_of_pass, presence: true, inclusion: { in: %w(monthly weekly daily none) }
+    validates :plate_number, presence: true, uniqueness: true
+    validates :start_date,   presence: true  
+    validates :end_date, presence: true
+    validate  :end_date_after_start_date
+
+
+    private
+    # Custome Validation
+    # create validation for start date always be before than end date
+    # create validation for end date is greter than start date
+    def end_date_after_start_date
+        return if start_date.blank? || end_date.blank?
+    
+        errors.add(:end_date, "must be after the start date") if end_date < start_date
+      end
+
+        
+    
+
+
+
+
 end
