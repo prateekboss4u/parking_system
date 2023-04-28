d80f057261cf9dd2fc4b69c18a1a109ab81e472e
prateeksikarwar <prateek612002@gmail.com>
2023-04-28 13:13:24 +0530

    Add Location Validation
    basic validation of all the objects

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,8 @@
 class Location < ApplicationRecord
+
+    validates :location_name, presence: true
+    validates :two_wheeler_capacity, numericality: { greater_than_or_equal_to: 0 }
+    validates :four_wheeler_capacity, numericality: { greater_than_or_equal_to: 0 }
+    validates :commercial_vehicle_capacity, numericality: { greater_than_or_equal_to: 0 }
+
 end
