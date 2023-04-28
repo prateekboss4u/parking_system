b86f5a22e12df37f17bc258a53664d2f310b1b68
prateeksikarwar <prateek612002@gmail.com>
2023-04-28 15:26:18 +0530

    Update Billing User
    added validations in Billing

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -1,2 +1,8 @@
 class Billing < ApplicationRecord
+
+    validates :customer_name, presence: true
+    validates :amount, presence: true
+    validates :duration, presence: true
+    validates :plate_number, presence: true
+
 end
