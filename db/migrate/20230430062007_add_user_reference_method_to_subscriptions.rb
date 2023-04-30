9b670d7f079808cafaf7d12cbb190e7eb8268d10
prateeksikarwar <prateek612002@gmail.com>
2023-04-30 16:52:16 +0530

    Update Subscription Refrences
    Chnaged Subscription refrence from User to Location

--- db/migrate/20230430062007_add_user_reference_method_to_subscriptions.rb
+++
@@ -1,5 +0,0 @@
-class AddUserReferenceMethodToSubscriptions < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :subscriptions, :user, null: false, foreign_key: true
-  end
-end
---
+++ db/migrate/20230430111847_add_location_reference_method_to_subscriptions.rb
@@ -0,0 +1,5 @@
+class AddLocationReferenceMethodToSubscriptions < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :subscriptions, :location, null: false, foreign_key: true
+  end
+end
