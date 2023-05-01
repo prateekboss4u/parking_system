4a51709dc2056fa65c23320649f4573a8db95ef9
ShelkePrajakta <prajakta.shelke20188@gmail.com>
2023-05-01 15:11:10 +0530

    updated Rate model
    update rate model

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -6,5 +6,17 @@ class Rate < ApplicationRecord
     validates :weekly_pass, presence: true, numericality: {greater_than:0}
     validates :monthly_pass, presence: true, numericality: {greater_than:0}
 
- 
+    #Regular Method
+    def calculate_cost(duration_in_hours)
+        case name
+        when 'Hourly'
+            hourly_rate * duration_in_hours
+        when 'Daily'
+            daily_rate * (duration_in_hours / 24.0).ceil
+        when 'Weekly'
+            weekly_rate * (duration_in_hours / 168.0).ceil
+        when 'Monthly'
+            monthly_rate * (duration_in_hours / 720.0).ceil
+        end
+    end
 end
