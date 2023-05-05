4cd8fc8f8cbcdf52a4f7a56f0f02f0582c5e5246
prateeksikarwar <prateek612002@gmail.com>
2023-05-05 09:14:31 +0530

    Update Subscription model & controller
    -added calculate_billing_amout to create billings
    -created bill_calculation method to caluculate (amount, duration)
    -added receipt method in controller to render bill receipt

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,8 @@
 class Subscription < ApplicationRecord
-    belongs_to :location
+    belongs_to :rate
     has_many :billings
+
+    after_create :bill_calculation
     
     
     validates :name, presence: true, exclusion: { in: %w(owner operator) }
@@ -9,6 +11,65 @@ class Subscription < ApplicationRecord
     validates :start_date,   presence: true  
     validates :end_date, presence: true
     validate  :end_date_after_start_date
+
+
+
+    def calculate_billing_amount(customer_name:name, amount: 0, duration:0, number_plate:plate_number)
+        
+        billings.create(subscription_id: id,
+                        customer_name: customer_name,
+                        amount: amount,
+                        duration: duration,
+                        plate_number: number_plate
+                        )
+    end
+
+    def bill_calculation
+        start_time = DateTime.parse(start_date.to_s)
+        end_time = DateTime.parse(end_date.to_s)
+        duration = ((end_time - start_time) * 24).ceil.to_i
+
+        hourly_rate = rate.hourly_rate
+        daily_rate = rate.daily_pass
+        weekly_rate = rate.weekly_pass
+        monthly_rate = rate.monthly_pass
+
+        total_cost = 0
+        if type_of_pass == 'none'
+            total_cost += (duration * hourly_rate)
+        elsif type_of_pass == 'daily'
+            reamining_time = (duration - 24)
+            if reamining_time <= 0
+                total_cost += daily_rate
+            else
+                total_cost += ((reamining_time*hourly_rate) + daily_rate)
+            end
+        elsif type_of_pass == 'weekly'
+            reamining_time = (duration - (24*7))
+            if reamining_time <= 0
+                total_cost += weekly_rate
+            else
+                total_cost += ((reamining_time*hourly_rate) + weekly_rate)
+            end
+        elsif type_of_pass == 'monthly'  
+            reamining_time = (duration - (24*7*4))
+            if reamining_time <= 0
+                total_cost += monthly_rate
+            else
+                total_cost += ((reamining_time*hourly_rate) + monthly_rate)
+            end
+        end
+
+        calculate_billing_amount(amount: total_cost, duration: duration)
+    end
+
+    def fetch_receipt
+        errors = []
+
+        return { status: false, transactions: nil, errors: errors } unless errors.blank?
+
+        return { status: true, transactions: billings.to_a, errors: nil }
+    end
 
 
     private
